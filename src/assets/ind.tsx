86a0dce64647b051189a5f47f056f464e47dd5ff
jatin-jamdagni <jatinsharma13arril2002@gmail.com>
2025-01-14 15:31:43 +0530

    first commit

---
+++ src/assets/ind.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const Ind = () => {
+  return (
+    <View>
+      <Text>ind</Text>
+    </View>
+  );
+};
+
+export default Ind;
---
+++ src/components/helo.tsx
@@ -0,0 +1,13 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import Ind from '@/assets/ind';
+const Hellow = () => {
+  return (
+    <View>
+      <Ind />
+      <Text>Hellow</Text>
+    </View>
+  );
+};
+
+export default Hellow;
